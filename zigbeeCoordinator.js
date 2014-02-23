f85eb3111634098ab3aa3a67389368b069759d05
mlamoure <mikel@mikelamoureux.net>
2014-02-23 11:53:33 -0500

    debugging updates to work with forked version of xbee-api

--- zigbeeCoordinator.js
+++ zigbeeCoordinator.js
@@ -1,6 +1,6 @@
 var util = require('util');
 var SerialPort = require('serialport').SerialPort;
-var xbee_api = require('xbee-api');
+var xbee_api = require('../xbee-api/lib');
 var path = require('path');
 var ZigbeeCoordinatorConfiguration = require("./zigbeeCoordinatorConfiguration.js");
 var configFileIncPath = path.join(__dirname + '/configuration.json');
